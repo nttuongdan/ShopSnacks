c75088b7927ac0a7bb83e4ce944f5f99f796ce94
tuongdan2002 <tuongdan2002@gmail.com>
2024-01-05 00:39:08 +0700

    2

--- src/main/java/DAO/MD5.java
+++ src/main/java/DAO/MD5.java
@@ -1,38 +1,36 @@
 package DAO;
 
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
 public class MD5 {
-public static String getMd5(String input)
- {
+	public static String getMd5(String input) {
 
- try {
+		try {
 
- // Static getInstance method is called with hashing MD5
- MessageDigest md = MessageDigest.getInstance("MD5");
+			// Static getInstance method is called with hashing MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
 
- // digest() method is called to calculate message digest
- // of an input digest() return array of byte
- byte[] messageDigest = md.digest(input.getBytes());
+			// digest() method is called to calculate message digest
+			// of an input digest() return array of byte
+			byte[] messageDigest = md.digest(input.getBytes());
 
- // Convert byte array into signum representation
- BigInteger no = new BigInteger(1, messageDigest);
+			// Convert byte array into signum representation
+			BigInteger no = new BigInteger(1, messageDigest);
 
- // Convert message digest into hex value
- String hashtext = no.toString(16);
- while (hashtext.length() < 32) {
- hashtext = "0" + hashtext;
- }
- return hashtext;
- }
+			// Convert message digest into hex value
+			String hashtext = no.toString(16);
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+			return hashtext;
+		}
 
- // For specifying wrong message digest algorithms
- catch (NoSuchAlgorithmException e) {
- throw new RuntimeException(e);
- }
- }
+		// For specifying wrong message digest algorithms
+		catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-} 
-
+}
